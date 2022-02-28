e49e086e36deebfd6b0fef46c9ad67141db78414
raidgar98 <raidgar98@onet.pl>
2022-02-28 23:29:55 +0100

    added logging, fixes in args and added warning about override of existing files

--- script1.0.py
+++ script1.0.py
@@ -4,15 +4,25 @@ import os
 from urllib import request
 from argparse import ArgumentParser
 from sys import argv
+from logging import Logger, INFO
+
+log = Logger(__file__, level=INFO)
+
+# CONST
+BOOL_PARAM = dict(nargs='?', type=bool, const=True, default=False)
 
 arg_engine = ArgumentParser()
-arg_engine.add_argument('-b','--book-id', type=int, target='book_id', required=True, help='id of book to download')
-arg_engine.add_argument('-p','--pages', type=int, target='pages', required=True, help='amount of pages to download')
+arg_engine.add_argument('-b', '--book-id', type=int, dest='book_id', required=True, help='id of book to download')
+arg_engine.add_argument('-p', '--pages', type=int, dest='pages', required=True, help='amount of pages to download')
+arg_engine.add_argument('-d', '--datadir', type=str, dest='datadir', default='pages', required=False, help='specifies directory where pages will be scrapped')
+arg_engine.add_argument('-f', '--force', dest='force', help='if given, overrides files in directory', **BOOL_PARAM)
 args = arg_engine.parse_args(list(argv[1:]))
 
 # ----------------------------------
-bookId = args.book_id # ID książki
-pages = args.pages # liczba stron książki
+bookId = args.book_id # book ID
+pages = args.pages # book page ccount
+datadir = args.datadir # path to directory with downloads
+override_files = args.force # override existing directory
 # ----------------------------------
 
 opener = request.build_opener()
@@ -21,7 +31,16 @@ request.install_opener(opener)
 pageText = "pages/page"
 fileExtension = ".PNG"
 
-os.mkdir("pages")
+if os.path.exists(datadir):
+	if not override_files:
+		log.warning('directory already exist, change path or remove existing one')
+		exit(-1)
+else:
+	try:
+		os.mkdir(datadir)
+	except Exception as e:
+		log.error(f'exception occured while creating directory: {e}')
+
 for i in range (1,int(pages) + 1):
   print("https://flipbook.apps.gwo.pl/book/getImage/bookId:" + str(bookId) + "/pageNo:" + str(i))
   filename = pageText + str(i) + fileExtension
